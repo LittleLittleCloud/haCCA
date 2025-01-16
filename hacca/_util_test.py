aaded5cb7957009640289435017dcd6dd9f40f14
XiaoYun Zhang <xiaoyuz@microsoft.com>
2025-01-15 22:54:39 -0800

    update

--- hacca/_util_test.py
+++ hacca/_util_test.py
@@ -1,5 +1,5 @@
 from .alignment import direct_alignment
-from .utils import _generate_mock_data, _plot_alignment_result
+from .utils import _generate_mock_data, plot_alignment_result
 
 def test_plot_alignment_result():
     # Create mock data
@@ -12,4 +12,4 @@ def test_plot_alignment_result():
     aligned_b = direct_alignment(a, b_prime, work_dir=None, enable_center_and_scale=False)
 
     # Call plot_alignment_result
-    _plot_alignment_result(a, b_prime, aligned_b, pi=None, title="Transport Plan", work_dir=None, show=True)
+    plot_alignment_result(a, b_prime, aligned_b, pi=None, title="Transport Plan", work_dir=None, show=True)
